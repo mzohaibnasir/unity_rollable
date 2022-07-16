7e9123ec3db6f9175db2e561fbf4622a8ca86a3c
Zohaib <m.zohaibnasir6@gmail.com>
2022-07-16 17:19:19 +0500

    Rollable -project

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset; //camera's offset value
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset =transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate() //for followcameras and gathering last known states //runs every frame like Update()
+    // but runs after all of the updates are done
+    {
+        transform.position=player.transform.position+offset;
+        //camera pos will only be set once player has moved from that frame
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed=10.0f;
+    public TextMeshProUGUI countText;
+
+
+
+    //as we are only enabling-deabling this text
+    public GameObject winTextObject;
+    private int count;
+
+    private Rigidbody rb;
+    private float movementX;
+    
+    private float movementY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb=GetComponent<Rigidbody>();
+        count=0;
+        SetCountText();
+        winTextObject.SetActive(false);
+    }
+
+    void onMove(InputValue movementValue){
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX=movementVector.x;
+        movementY=movementVector.y;
+        Debug.Log("onMove()");
+
+    }
+
+    void SetCountText(){
+        countText.text= "Count: " + count.ToString();
+        if (count>=2)
+        {
+                          winTextObject.SetActive(true);
+      
+        }
+    }
+
+    void FixedUpdate(){
+        Vector3 movement = new Vector3(movementX,0.0f,movementY);
+        rb.AddForce(movement*speed);  
+    }
+
+    void OnTriggerEnter(Collider other){
+        if (other.gameObject.CompareTag("pickUp"))
+        {
+            other.gameObject.SetActive(false);//disabling object
+            count=count+1;
+            SetCountText();
+
+        }
+    }
+    
+
+
+   
+}
