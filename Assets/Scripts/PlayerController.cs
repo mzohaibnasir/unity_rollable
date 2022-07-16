80f05477479068f9dc71b4ac0a5b79cc92099c2e
Zohaib <m.zohaibnasir6@gmail.com>
2022-07-16 20:25:32 +0500

    After jump addition

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,18 +7,27 @@ using TMPro;
 public class PlayerController : MonoBehaviour
 {
     public float speed=10.0f;
+    public float jumpForce=10.0f;
     public TextMeshProUGUI countText;
+    private bool isGrounded;
+
+    // public 
 
 
 
     //as we are only enabling-deabling this text
     public GameObject winTextObject;
     private int count;
-
     private Rigidbody rb;
     private float movementX;
-    
     private float movementY;
+    private string GROUND_TAG = "Ground";
+
+
+
+    public void Awake(){
+        // PlayerInputActions inputAction = new PlayerInputActions();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -35,6 +44,16 @@ public class PlayerController : MonoBehaviour
         Debug.Log("onMove()");
 
     }
+    void OnJump(){ //onAction
+        if (isGrounded)
+        {
+            rb.AddForce(new Vector3(0f, 10.0f, 0f),ForceMode.Impulse);
+            Debug.Log("Jumped");
+            isGrounded=false;   
+        }
+    }
+
+
 
     void SetCountText(){
         countText.text= "Count: " + count.ToString();
@@ -48,7 +67,10 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate(){
         Vector3 movement = new Vector3(movementX,0.0f,movementY);
         rb.AddForce(movement*speed);  
+        // playerJump();
     }
+
+    
 
     void OnTriggerEnter(Collider other){
         if (other.gameObject.CompareTag("pickUp"))
@@ -59,6 +81,13 @@ public class PlayerController : MonoBehaviour
 
         }
     }
+
+     private void OnCollisionEnter(Collision other) {
+        if (other.gameObject.CompareTag(GROUND_TAG))
+        {
+            isGrounded=true;
+        }
+     }
     
 
 
