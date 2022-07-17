da7e661d50a9283019fdb04904a2173ab58f0cd1
Zohaib <m.zohaibnasir6@gmail.com>
2022-07-17 12:49:35 +0500

    +scooker-scene

---
+++ Assets/Collector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collector : MonoBehaviour
+{
+
+    public GameObject winTextObject;
+    public  string OTHERBALLSTAG= "OtherBalls";
+    
+    // Start is called before the first frame update
+    void Start(){
+    }
+
+void OnTriggerEnter(Collider other){
+        if(other.gameObject.CompareTag(OTHERBALLSTAG)){
+                winTextObject.SetActive(true);
+                // Debug.Log("SCORED");
+        }
+
+        
+
+}
+
+void OnTriggerExit(Collider other){
+            winTextObject.SetActive(false);
+            // Debug.Log("SCORED");
+    }
+    
+}
---
+++ Assets/Scripts/SnookerPlayerController.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class SnookerPlayerController : MonoBehaviour
+{
+    public float speed=10.0f;
+    public float jumpForce=10.0f;
+    public TextMeshProUGUI countText;
+    private bool isGrounded;
+    private Color boxColor,ballColor;
+    private Color redColor= new Color(1.0f, 0.0f, 0.0f, 1.0f);
+    private Color greenColor= new Color(0.0f, 1.0f, 0.0f, 1.0f);
+    private Color blueColor= new Color(0.0f, 0.0f, 1.0f, 1.0f);
+
+    // public 
+
+
+
+    //as we are only enabling-deabling this text
+    public GameObject winTextObject;
+    private int count;
+    private int redBoxCount;
+    private int greenBoxCount;
+    private int blueBoxCount;
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    private string GROUND_TAG = "Ground";
+
+
+
+    public void Awake(){
+        // PlayerInputActions inputAction = new PlayerInputActions();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb=GetComponent<Rigidbody>();
+        count=0;
+        redBoxCount=0;
+        greenBoxCount=0;
+        blueBoxCount=0;
+        SetCountText();
+        winTextObject.SetActive(false);
+    }
+
+    void OnMove(InputValue movementValue){
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX=movementVector.x;
+        movementY=movementVector.y;
+        // Debug.Log("onMove()");
+
+    }
+    void OnJump(){ //onAction
+        if (isGrounded)
+        {
+            rb.AddForce(new Vector3(0f, 10.0f, 0f),ForceMode.Impulse);
+            // Debug.Log("Jumped");
+            isGrounded=false;   
+        }
+    }
+
+
+
+    void SetCountText(){
+        countText.text= "Red:" + redBoxCount.ToString() + "-Blue:" + blueBoxCount.ToString() + "-Green:" + greenBoxCount.ToString();
+        if (greenBoxCount>=1 && redBoxCount>=2 && blueBoxCount>=1  )
+        {
+            winTextObject.SetActive(true);
+    
+        }
+    }
+
+    void FixedUpdate(){
+        Vector3 movement = new Vector3(movementX,0.0f,movementY);
+        rb.AddForce(movement*speed);  
+        // playerJump();
+    }
+
+    
+
+    void OnTriggerEnter(Collider other){
+        if (other.gameObject.CompareTag("pickUp"))
+        {
+            
+            other.gameObject.SetActive(false);//disabling object
+            ballColor = gameObject.GetComponent<Renderer>().material.color;
+            boxColor = other.gameObject.GetComponent<Renderer>().material.color;
+            // boxColor= new Color(1.0f, 0.0f, 0.0f, 1.0f);
+            
+
+            if(boxColor.r==1.0f){
+                // Debug.Log("Red");
+                redBoxCount++;
+            }
+            if(boxColor.b==1.0f){
+                // Debug.Log("Blue");
+                blueBoxCount++;
+            }
+            if(boxColor.g==1.0f){
+                // Debug.Log("Green");
+                greenBoxCount++;
+            }
+            
+            gameObject.GetComponent<Renderer>().material.color = ballColor + boxColor;
+            // count=count+1;
+            SetCountText();
+
+
+        }
+    }
+
+     private void OnCollisionEnter(Collision other) {
+        if (other.gameObject.CompareTag(GROUND_TAG))
+        {
+            isGrounded=true;
+        }
+     }
+
+     private void OnCollisionStay(Collision other){
+        Rigidbody otherRigidbody = other.gameObject.GetComponent<Rigidbody>();
+        // other.gameObject.GetComponent<Rigidbody>().AddForce(40, 40, 41);
+        if(otherRigidbody){
+            Debug.Log("Collision");
+            Vector3 forceDirection = other.gameObject.transform.position - transform.position;
+            forceDirection.y=0;
+            // otherRigidbody.AddForce(forceDirection * 10.0f, ForceMode.Acceleration);
+            otherRigidbody.AddForce(forceDirection * 5.0f, ForceMode.Impulse);
+            // otherRigidbody.AddForce(forceDirection * 5.0f, ForceMode.Force);
+
+         }
+     }
+    
+
+
+   
+}
